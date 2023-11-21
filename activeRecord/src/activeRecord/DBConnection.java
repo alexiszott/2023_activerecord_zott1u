fe7d67983408aaf96ea484b3f6720c73e22f02a5
Alexis <120113186+KaiizokuOu@users.noreply.github.com>
2023-11-21 15:18:31 +0100

    add singleton

---
+++ activeRecord/src/activeRecord/DBConnection.java
@@ -0,0 +1,33 @@
+package activeRecord;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
+public class DBConnection {
+    private static final String USER_NAME = "root";
+    private static final String PASSWORD = "";
+    private static final String SERVER_NAME = "localhost";
+    private static final String PORT_NUMBER = "3306";
+    private static String dbName = "testpersonne";
+    private static Connection co;
+
+    // creation de la connection
+    public static Connection getConnection() throws SQLException {
+        if(co == null){
+            Properties connectionProps = new Properties();
+            connectionProps.put("user", USER_NAME);
+            connectionProps.put("password", PASSWORD);
+            String urlDB = "jdbc:mysql://" + SERVER_NAME + ":";
+            urlDB += PORT_NUMBER + "/" + dbName;
+            co = DriverManager.getConnection(urlDB, connectionProps);
+        }
+        return co;
+    }
+
+    public void setNomDB(String nomDB) throws SQLException {
+        dbName = nomDB;
+        co = getConnection();
+    }
+}
